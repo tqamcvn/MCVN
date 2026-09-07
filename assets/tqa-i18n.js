2c5142814c339bfde4547a5fe3b095ed22565727
Phan Nguyễn Yến Nhi <akkona197@gmail.com>
2026-09-07 17:47:43 +0700

    Restore Home navigation and remove National Day decorations

--- assets/tqa-i18n.js
+++ assets/tqa-i18n.js
@@ -63,7 +63,6 @@
     'Mở tab mới': 'Open in new tab',
     'Đang phát triển': 'In development',
     'Tính năng đang phát triển': 'This feature is under development',
-    'Chào mừng Quốc khánh': 'Vietnam National Day',
     'Chủ tịch Hồ Chí Minh': 'President Ho Chi Minh',
     'cập nhật': 'updated',
     'đệm': 'cached',
